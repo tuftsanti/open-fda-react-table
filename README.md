60609e6ed5890f637650e61bbad3299d5297c5b7
Andy Riecker <ariecker@gmail.com>
2021-02-28 17:37:50 -0500

    finished README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# kna
+## Open FDA API in a React Table
+Importing Drug information from the OpenFDA API into a table
+
+Hosted @: https://open-fda-react-table.netlify.app
+
+### Beginnings
+
+I've wanted to share my React Table skills without a backend, so I decided to read the FDA's drug data directly from the API and pass the drug info directly into a table. I realize RT7 has been out for a while, but this was built in RT6 because that's what I'd learned initially. I've used my own API key from OpenFDA in the hosted version, so you'd need your own from here to run locally. Simple go to https://open.fda.gov/apis/authentication/ and click the "Get API key" button. Once you have it, create a .env file in the root of the clone folder with only this line:
+
+REACT_APP_OPEN_FDA_KEY=*yourkeyhere*
+
+replacing *yourkeyhere* with your key from OpenFDA
+
+then install your packages (npm i) and start the local build (npm run start)
+
+### Middles
+
+Because there are strict limitations on free users calling the API (registered users with a key face a *slightly* easier task), I limited the call to 500 total drugs. The page defaults to showing 25 total drugs and their few properties, and pages can be manipulated from the top or bottom of the table. Every column is dual directionally sortable by clicking on the column title. It is also searchable by typing in the search box atop each column. Columns have a default nimimum width but can be altered on the fly. The columns can even be dragged by their header and reordered.
+
+### Endings
+
+This was built in React using Table and a lot more props than I'd like to admit. I'd initially planned on using Axios for the call, but fetching was much simpler on a GET call. Apart from the basic react packages, I used a bit of bootstrap for styling and react-table-hoc-draggable-columns to allow for reordering the columns. There's also styled-components for the overall look of the table.
+
+Ultimately this was a fun project and I'm glad I was able to show off my RT skills!
--- src/routes/List.js
+++ src/routes/List.js
@@ -3,8 +3,13 @@ import ReactTable from 'react-table'
 import '../style/style.css'
 import styled from 'styled-components'
 import 'react-table/react-table.css'
+
+import withDraggableColumns from 'react-table-hoc-draggable-columns';
+import 'react-table-hoc-draggable-columns/dist/styles.css';
 // console.log(process.env.REACT_APP_OPEN_FDA_KEY)
 const OPENFDAKEY = process.env.REACT_APP_OPEN_FDA_KEY;
+
+const ReactTableDraggableColumns = withDraggableColumns(ReactTable);
 
 const Table = styled.div`
     padding: 0 40px 40px 40px;
@@ -20,7 +25,7 @@ class List extends Component {
     }
 
     componentDidMount() {
-        fetch(`https://api.fda.gov/drug/drugsfda.json?api_key=${OPENFDAKEY}&limit=5`)
+        fetch(`https://api.fda.gov/drug/drugsfda.json?api_key=${OPENFDAKEY}&limit=500`)
         .then(res => res.json())
         .then((data) => {
             this.setState({ drugs: data.results})
@@ -28,6 +33,7 @@ class List extends Component {
         })
         .catch(console.log)
     }
+
     filterCaseInsensitive = (filter, row) => {
         const id = filter.pivotId || filter.id;
         const content = row[id];
@@ -54,6 +60,9 @@ class List extends Component {
                 accessor: 'application_number',
                 filterable: true,
                 minWidth: 120,
+                // Filter: () => (
+                //     <input placeholder={'Search by Application..'} />
+                // ),
             },
             {
                 Header: 'Sponsor Name',
@@ -61,12 +70,18 @@ class List extends Component {
                 filterable: true,
                 sortable: true,
                 minWidth: 250,
+                // Filter: () => (
+                //     <input placeholder={'Search by Sponsor..'} />
+                // )
             },
             {
                 Header: 'Reference Drug',
                 accessor: 'products[0].reference_drug',
                 filterable: true,
                 minWidth: 150,
+                // Filter: () => (
+                //     <input placeholder={'Search by Reference..'} />
+                // ),
                 getProps: (state, rowInfo, column) => {
                     return {
                         style: {
@@ -82,24 +97,36 @@ class List extends Component {
                 accessor: 'products[0].brand_name',
                 filterable: true,
                 minwidth: 250,
+                // Filter: () => (
+                //     <input placeholder={'Search by Brand..'} />
+                // )
             },
             {
                 Header: 'Dosage Form',
                 accessor: 'products[0].dosage_form',
                 filterable: true,
-                minWidth: 150
+                minWidth: 150,
+                // Filter: () => (
+                //     <input placeholder={'Search by Dosage..'} />
+                // )
             },
             {
                 Header: 'Route',
                 accessor: 'products[0].route',
                 filterable: true,
-                minWidth: 150
+                minWidth: 150,
+                // Filter: () => (
+                //     <input placeholder={'Search by Route..'} />
+                // )
             },
             {
                 Header: 'Marketing Status',
                 accessor: 'products[0].marketing_status',
                 filterable: true,
-                minWidth: 150
+                minWidth: 150,
+                // Filter: () => (
+                //     <input placeholder={'Search by Status..'} />
+                // )
             },
         ]
 
@@ -112,8 +139,19 @@ class List extends Component {
         return (
             <Table >
                 {showTable && (                  
-                    <ReactTable
-                        className="-highlight" 
+                    <ReactTableDraggableColumns
+                        draggableColumns= {{
+                            mode: 'reorder',
+                            enableColumnWideDrag: true,
+                            draggable: ['application_number', 
+                                        'sponsor_name', 
+                                        'products[0].reference_drug', 
+                                        'products[0].brand_name', 
+                                        'products[0].dosage_form',
+                                        'products[0].route',
+                                        'products[0].marketing_status'],
+                        }}
+                        className='-highlight' 
                         data={drugs}
                         columns={columns}
                         loading={isLoading}
